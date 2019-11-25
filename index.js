f3ebf4c797ddfeabceaeb4d7aec7516c93a7e473
Thiago <thiagopap@gmail.com>
2019-11-24 23:24:47 -0300

    Bootcamp - NodeJs Concepts

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+//Query params = ?teste=1
+//Route params = /users/1
+//Request body = { name: "Thiago", "email": thiagopap@gmail.com }
+
+//localhost:3000/users/12345
+
+//CRUD - Create, Read, Update, Delete
+
+const users = ["Thiago", "Diego", "Robson"];
+
+//Middleware Global
+server.use((req, res, next) => {
+  console.time("Request");
+  console.log(`Método ${req.method}; URL: ${req.url}`);
+
+  next();
+
+  console.log("finalizou");
+
+  console.timeEnd("Request");
+});
+
+//Middlewares Locais
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "User name is required." });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+
+  if (!user) {
+    return res.status(400).json({ error: "User does not exists." });
+  }
+
+  req.user = user;
+
+  return next();
+}
+
+//Routes
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  res.json(req.user);
+});
+
+server.post("/users", checkUserExists, (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", checkUserInArray, checkUserExists, (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", checkUserInArray, (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+});
+
+server.listen(3000);
